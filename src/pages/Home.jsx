3564822b393e172e892595aec18aa1c038156f8f
askesis <asketes@milkandcartoons.com>
2018-10-08 15:47:01 +0300

    Last meeting

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Query } from 'react-apollo';
 import { concatAST } from 'graphql'
 
+import BOOKS_QUERY from '../graphql/books';
 import ME from '../graphql/queries/me';
 import USERS from '../graphql/queries/users'
 import USER_FRAGMENT from '../graphql/fragments/user';
@@ -14,14 +15,23 @@ class Home extends Component {
       <React.Fragment>
         <h1>JSC</h1>
 
-        <Query query={concatAST([USERS, USER_FRAGMENT])} >
+      <Query query={BOOKS_QUERY}>
+        {({ error, loading, data }) => {
+          if ( error ) {console.error(error);return;}
+
+          if ( loading ) return <p>Loading...</p>
+          
+          return data.books.map( book => <div key={book.id}>{book.title} {book.author.firstName} {book.author.firstName}</div>)
+        }}
+      </Query>
+        {/*<Query query={concatAST([USERS, USER_FRAGMENT])} >
           { ({ error, loading, data, refetch }) => {
             if (error) console.log(error);
             if (loading) return <p>Loading... </p>
             console.log(data);
             return <UsersList users={data.allUsers} refetch={refetch} />
           }}
-        </Query>
+        </Query>*/}
       </React.Fragment>
     );
   }
